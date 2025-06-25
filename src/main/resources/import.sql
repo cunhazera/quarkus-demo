f2b1fb1b3b3a3900507fb6b91f5df71f7d531e89
Gabriel Cunha <gabriel.cunha@ninjarmm.com>
2025-06-25 17:28:52 -0300

    New version

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO todo (id, title, priority, done) values (1,'Wake up', 10, false);
-INSERT INTO todo (id, title, priority, done) values (2,'Breakfast', 5, false);
-INSERT INTO todo (id, title, priority, done) values (3,'Work', 10, false);
+INSERT INTO todo (id, title, priority, done) values (111,'Wake up', 10, false);
+INSERT INTO todo (id, title, priority, done) values (1111,'Breakfast', 5, false);
+INSERT INTO todo (id, title, priority, done) values (333,'Work', 10, false);
